c5e9a592df9dbb88b3f72a810be21ce9ee28ba40
Toby <wsgtracy@163.com>
2017-05-19 08:31:39 +0800

    add layout

--- app.js
+++ app.js
@@ -18,14 +18,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
 app.use(session({
   secret: settings.cookieSecret,
   key: settings.db, // cookie name
@@ -34,8 +26,17 @@ app.use(session({
     db: settings.db,
     host: settings.host,
     port: settings.port
-  });
+  })
 }));
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 
 app.use('/', index);
 app.use('/users', users);

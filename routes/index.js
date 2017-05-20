5a2914f7b81ef8d3886adaca8709f5afdc7e71b6
Toby <wsgtracy@163.com>
2017-05-20 17:02:47 +0800

    实现页面权限控制

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,8 @@ router.get('/', function(req, res, next) {
   })
 
 });
+
+router.get('/reg', checkNotLogin)
 router.get('/reg', function(req, res) {
   res.render('reg', {
     title: '注册',
@@ -28,23 +30,47 @@ router.get('/reg', function(req, res) {
   });
 });
 
+router.post('/reg', checkNotLogin);
 router.post('/reg', doReg);
 
+router.get('/login', checkNotLogin);
 router.get('/login', function(req, res) {
   res.render('login', {
     title: '登录',
     user: req.session.user
   });
 });
+
+router.post('/login', checkNotLogin);
 router.post('/login', doLogin);
+
+router.get('/post', checkLogin);
 router.get('/post', function(req, res) {
   res.render('post', {
     title: '发表',
     user: req.session.user
   });
 });
+
+router.post('/post', checkLogin);
 router.post('/post', doPost);
 
+router.get('/logout', checkLogin);
 router.get('/logout', doLogout);
 
+// 判断是否登录
+function checkLogin (req, res, next) {
+  if (!req.session.user) {
+    res.redirect('/login');
+  }
+  next();
+}
+
+// 判断是否已经登录
+function checkNotLogin (req, res, next) {
+  if (req.session.user) {
+    res.redirect('back'); // 返回之前的页面
+  }
+  next();
+}
 module.exports = router;
